790bc71f46d7c5faf66eaf9df8f2ed400598db09
haydar kose <haydar.kose@saritay.com>
2025-05-15 18:11:11 +0300

    added swagger ui. downgraded spring boot to 3.3.3

---
+++ services/customer-service/src/main/java/com/greenearn/customerservice/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package com.greenearn.customerservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth Service API")
+                        .description("Green Earn Auth Service API Documentation")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Green Earn Team")
+                                .email("contact@greenearn.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8083")
+                                .description("Local Development Server")
+                ))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+} 
