2699df679458af765a378850dece731f1447fb1f
haydar kose <haydar.kose@saritay.com>
2025-05-19 12:40:56 +0300

    updated package name

--- services/auth-service/src/test/java/com/greenearn/authservice/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/greenearn/authservice/AuthServiceApplicationTests.java
@@ -1,7 +1,8 @@
-package com.ecommerce.authservice;
+package com.greenearn.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class AuthServiceApplicationTests {
--- services/mail-service/src/test/java/com/greenearn/mailservice/MailServiceApplicationTests.java
+++ services/mail-service/src/test/java/com/greenearn/mailservice/MailServiceApplicationTests.java
@@ -1,7 +1,8 @@
-package com.ecommerce.mailservice;
+package com.greenearn.mailservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class MailServiceApplicationTests {
