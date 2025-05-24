8c6103bc652ccf4a87438f4e03ae70d290af5a58
haydar kose <haydar.kose@saritay.com>
2025-05-24 21:19:25 +0300

    updated

---
+++ services/challenge-service/src/main/java/com/greenearn/challengeservice/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.greenearn.challengeservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${openapi.server.url}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Challenge Service API")
+                        .description("Green Earn Challenge Service API Documentation")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Green Earn Team")
+                                .email("contact@greenearn.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url(serverUrl)
+                                .description("Local Development Server")
+                ))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+} 
